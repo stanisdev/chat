45a70758647f9e01fd65175f53fdfec13c754a9c
stanisdev <javascript.nodejs.developer@gmail.com>
2020-10-13 18:32:11 +0600

    Rewriting of the core error handler

--- src/loaders/errors.js
+++ src/loaders/errors.js
@@ -1,47 +1,67 @@
 'use string'
 
 const fp = require('fastify-plugin');
+const status = require('http-status');
+const isProduction = process.env.NODE_ENV === 'production';
 
 async function errors(fastify) {
-  fastify.setErrorHandler(function (error, request, reply) {
-    fastify.log.error(error);
+  fastify.setErrorHandler((error, request, reply) => {
 
-    if (error instanceof Error) {
-      const payload = {
-        ok: false,
-        errors
-      };
-      const { validation: ajvValidation } = error;
+    let errors = [];
+    let code, message, logging = false;
+    /**
+     * If an error was thrown by AJV validator
+     */
+    if (Array.isArray(error.validation)) {
+      code = status.BAD_REQUEST;
+      message = status[code];
 
-      /**
-       * Boom's error
-       */
-      if (error.isBoom) {
-        const { statusCode, message: msg } = error.output.payload;
-        let message = null;
-        try {
-          payload.errors = JSON.parse(msg);
-        } catch (err) {
-          message = msg;
-        }
-        if (typeof message === 'string') {
-          payload.message = message;
-        }
-        return reply.code(statusCode).send(payload);
-      }
-      /**
-       * Ajv validation error
-       */
-      else if (Array.isArray(ajvValidation) && ajvValidation.length > 0) {
-        ajvValidation.forEach(({ dataPath, message }) => {
-          payload.errors[dataPath.slice(1)] = message;
-        });
-        return reply.code(400).send(payload);
-      }
+      errors = error.validation.map(({ params, message, dataPath }) => {
+        const field = dataPath.length > 0 ? dataPath : Object.values(params)[0];
+        return {
+          field: field.split('.').filter(e => e.length > 0)[0],
+          message
+        };
+      });
     }
-    reply.code(500).send({
-      ok: false,
-      message: 'Unknown Server Error'
+    /**
+     * If an error was thrown by the Boom
+     */
+    else if (error.isBoom) {
+      let { statusCode, message: json } = error.output.payload;
+      try {
+        json = JSON.parse(json);
+      } catch {
+        json = {};
+      }
+      errors = Object.keys(json).map(field => {
+        return {
+          field,
+          message: json[field]
+        };
+      });
+      code = statusCode;
+      message = status[code];
+    }
+    /**
+     * Unknown case
+     */
+    else if (error instanceof Error) {
+      code = status.INTERNAL_SERVER_ERROR;
+      message = isProduction ? status[code] : error.message;
+      logging = true;
+    }
+    else {
+      code = status.INTERNAL_SERVER_ERROR;
+      logging = true;
+    }
+    if (logging) {
+      fastify.log.error(error);
+    }
+    reply.code(code).send({
+      code,
+      message,
+      errors
     });
   });
 }
